9944da9f0bb5d8848ec4b5b9b20d3a013836990b
TarunChapman <tarun@optigenomics.bio>
2025-06-23 10:09:56 +0100

    Pre AI font add

--- src/main/java/com/tc/reactor/Main.java
+++ src/main/java/com/tc/reactor/Main.java
@@ -5,13 +5,16 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, FontFormatException {
+        Font JetBrainsMono = Font.createFont(Font.TRUETYPE_FONT, Objects.requireNonNull(getClass().getResourceAsStream("fonts/JetBrainsMono-Regular.ttf")));
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("fxml/MainView.fxml"));
         Scene scene = new Scene(loader.load());
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("css/styles.css")).toExternalForm());
