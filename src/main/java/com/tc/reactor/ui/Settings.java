af436201efe639ba5fa4fa2424cf053552176754
theonetruenerd <tarunchapman@hotmail.com>
2025-07-19 13:43:59 +0100

    REA-8 Create Settings Window
    Removed stack pane, made it so that cancel button closes the settings window

--- src/main/java/com/tc/reactor/ui/Settings.java
+++ src/main/java/com/tc/reactor/ui/Settings.java
@@ -1,4 +1,43 @@
 package com.tc.reactor.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class Settings {
+
+    @FXML private MenuBar settingsMenuBar;
+    @FXML private ButtonBar settingsButtonBar;
+    @FXML private Button settingsApplyButton;
+    @FXML private Button settingsCancelButton;
+    @FXML private ListView<String> settingsListView;
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    private void applySettings() {
+
+    }
+
+    private void cancelSettings() {
+        Stage stage = (Stage) settingsButtonBar.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void onSettingsApplyButtonClick() {
+        applySettings();
+    }
+
+    @FXML
+    private void onSettingsCancelButtonClick() {
+        cancelSettings();
+    }
+
 }
