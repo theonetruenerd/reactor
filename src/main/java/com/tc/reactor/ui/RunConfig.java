e4d4497663619884b7339366f1a80dd419be62cc
TarunChapman <tarun@optigenomics.bio>
2025-07-22 10:33:46 +0100

    Added functionality to exe browse button

--- src/main/java/com/tc/reactor/ui/RunConfig.java
+++ src/main/java/com/tc/reactor/ui/RunConfig.java
@@ -4,7 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class RunConfig {
 
@@ -24,5 +27,20 @@ public class RunConfig {
         System.out.println("Cancel button clicked.");
         Stage stage = (Stage) exeComboBox.getScene().getWindow();
         stage.close();
+    }
+
+    @FXML
+    private void onExeBrowseButtonClick() {
+        System.out.println("Browse button clicked.");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select executable file");
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Executable files (*.exe)", "*.exe");
+        fileChooser.getExtensionFilters().add(filter);
+        File initialFile = new File("C:\\Program Files (x86)\\HAMILTON\\Bin");
+        fileChooser.setInitialDirectory(initialFile);
+        File selectedExe = fileChooser.showOpenDialog(new Stage());
+        if (selectedExe != null) {
+            exeComboBox.setValue(selectedExe.getAbsolutePath());
+        }
     }
 }
