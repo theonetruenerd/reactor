5ff68c1ebb220bde6ef6f7c6fc966808ba578cf1
theonetruenerd <tarunchapman@hotmail.com>
2025-07-29 20:38:35 +0100

    fixed break

--- src/main/java/com/tc/reactor/ui/ReadOnly.java
+++ src/main/java/com/tc/reactor/ui/ReadOnly.java
@@ -13,19 +13,14 @@ public class ReadOnly {
     @FXML
     private void onYesButtonClicked() {
         isReadOnly = true;
-        yesButton.setDisable(true);
-        noButton.setDisable(true);
         Stage stage = (Stage) yesButton.getScene().getWindow();
         stage.close();
     }
     @FXML
     private void onNoButtonClicked() {
         isReadOnly = false;
-        yesButton.setDisable(false);
-        noButton.setDisable(false);
         Stage stage = (Stage) noButton.getScene().getWindow();
         stage.close();
-        // test
     }
 
     public boolean isReadOnly() {
